8ec72dd627ff0fc48e69a1abeecd620b97ee07f5
noviomagus-lky <s1891130@ed.ac.uk>
2019-09-27 16:49:42 +0100

    version1.0 implement class direction with enum

--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Direction.java
@@ -1,23 +1,7 @@
 package uk.ac.ed.inf.powergrab;
 
-public class Direction {
-
-	static private double pi = Math.PI;
-	static double E = 0.0;
-	static double ENE = pi/8;
-	static double NE = pi/4;
-	static double NNE = 3*pi/8;
-	static double N = pi/2;
-	static double NNW = 5*pi/8;
-	static double NW = 3*pi/4;
-	static double WNW = 7*pi/8;
-	static double W = pi;
-	static double WSW = -7*pi/8;
-	static double SW = -3*pi/4;
-	static double SSW = -5*pi/8;
-	static double S = -pi/2;
-	static double SSE = -3*pi/8;
-	static double SE = -pi/4;
-	static double ESE = -pi/8;
+public enum Direction {
+    
+	E,ENE,NE,NNE,N,NNW,NW,WNW,W,WSW,SW,SSW,S,SSE,SE,ESE
 	
 }
--- powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
+++ powergrab/src/main/java/uk/ac/ed/inf/powergrab/Position.java
@@ -14,10 +14,33 @@ public class Position {
 		
 	}
 	
-	public Position nextPosition(double direction)
+	public Position nextPosition(Direction direction)
 	{
-		double nextLatitude = latitude + moveUnitDistance*Math.sin(direction);
-	    double nextLongitude = longitude + moveUnitDistance*Math.cos(direction);
+		double pi = Math.PI;
+	    double angle = 0;
+		switch(direction) {
+		
+		
+		case E : angle= 0.0;break;
+		case ENE : angle= pi/8;break;
+		case NE : angle= pi/4;break;
+		case NNE : angle= 3*pi/8;break;
+		case N : angle= pi/2;break;
+		case NNW : angle= 5*pi/8;break;
+		case NW : angle= 3*pi/4;break;
+		case WNW : angle= 7*pi/8;break;
+		case W : angle= pi;break;
+		case WSW : angle= -7*pi/8;break;
+		case SW : angle= -3*pi/4;break;
+		case SSW : angle= -5*pi/8;break;
+		case S : angle= -pi/2;break;
+		case SSE : angle= -3*pi/8;break;
+		case SE : angle= -pi/4;break;
+		case ESE : angle= -pi/8;break;
+		default: angle = 0.0;
+		}
+		double nextLatitude = latitude + moveUnitDistance*Math.sin(angle);
+	    double nextLongitude = longitude + moveUnitDistance*Math.cos(angle);
 	    return new Position(nextLatitude,nextLongitude);
 	}
 	
